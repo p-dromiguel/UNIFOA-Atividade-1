d3506da70499f9ecb991789817d2f568887b2b37
p-dromiguel <0pedro.miguel05@gmail.com>
2024-08-10 11:37:22 -0700

    100anos.py

--- 100anos.py
+++ 100anos.py
@@ -1,28 +1,34 @@
-import time 
-nome = input("Olá, qual o seu nome?")
-time.sleep(2)   
-print (f"Belíssimo nome, {nome}!")
+import time #Biblioteca time importada, para dar espaçamento entre as respostas. 
+import sys #atualização com o sys importado, para encerramento do programa.
+
+nome = input("Olá, qual o seu nome?") #primeira vez que input foi criado, com a variável nome.
+time.sleep(2) #tempo de dois segundos, pela  
+print (f"Belíssimo nome, {nome}!") #print sendo usada para transmitir a mensagem para o código.
 time.sleep(2)
-while True: 
- idade = input("Qual sua idade?")
- try: 
-   idade = int(idade)
-   break 
- except ValueError:
+while True:  #Como na própria tradução, enquanto verdade, e o usuário fizer corretamente, colocando uma idade como número inteiro, o código seguirá em frente.
+ idade = input("Qual sua idade?") 
+ try:   #É uma tentativa de um prosseguimento correto.
+   idade = int(idade)   #int para garantir que seja um número inteiro
+   break     #break para encerrar o ciclo
+ except ValueError: #com exceção do erro de o valor por acaso não ter sido um número inteiro.
   print(f"Ou você não me deu sua idade em número ou foi em algarismo romano! Fale sua idade em um número inteiro!")
 time.sleep(1)
 print (f"É uma ótima idade!{idade} aninhos só!")
 resposta = input("Você gostaria de saber quantos anos faltam para você completar 100? Sim ou não?")
 time.sleep(1)
-if resposta.lower() == "sim":
+if resposta.lower() == "sim": #caso a resposta seja sim, 
     print (f"Vamos lá!")
 else:
-    print (f"Você não teria escolha mesmo!")
+    print (f"Beleza então. Caso mude de ideia, inicie novamente o programa. Em 5 segundos, ele será encerrado!")
+    time.sleep(5)
+    sys.exit() #Programa descoberto enquanto estava fazendo outra atividade. Esse sys que foi importado serve para essa função.
 time.sleep(2)
 anos_faltantes = 100 - idade
 if idade <= 100:
  print (f"Faltam apenas {anos_faltantes} para você completar 100 anos!")
 else:
- print (f"Você é bem velho, ein! Já passou da idade!")
+ print (f"Você é bem velho, ein! Ou já passou da idade ou já tem 100 anos!")
  
- 
+ print(f"O programa será encerrado em 5 segundos. Espero que você tenha ficado feliz com o tempo que tem restante. Ou não!")
+ time.sleep(5)
+ sys.exit()
